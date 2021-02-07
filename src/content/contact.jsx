4bb82af93d3f6a19892343e328392bfd4a80f4c9
Curt Henrichs <cdhenrichs@wisc.edu>
2021-02-07 16:16:07 -0600

    initial content structure + further implementation of view - contacts layout needs adjustment

---
+++ src/content/contact.jsx
@@ -0,0 +1,25 @@
+
+const data = {
+  "github": {
+    "link": "https://github.com/curthenrichs",
+    "text": null
+  },
+  "linkedin": {
+    "link": "https://www.linkedin.com/in/curt-henrichs/",
+    "text": null
+  },
+  "email": {
+    "link": "mailto:curthenrichs@gmail.com",
+    "text": "curthenrichs@gmail.com"
+  },
+  "telephone": {
+    "link": "tel:+1262-422-7274",
+    "text": "+1 262-422-7274"
+  },
+  "location": {
+    "link": null,
+    "text": "Madison, Wisconsin, USA"
+  }
+};
+
+export default data;
