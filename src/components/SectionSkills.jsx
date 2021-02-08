93c80726e235fdce4dc079f3d57544d224940d11
Curt Henrichs <cdhenrichs@wisc.edu>
2021-02-07 22:48:16 -0600

    Adding skill related icons + wrote out first draft of skills data

--- src/components/SectionSkills.jsx
+++ src/components/SectionSkills.jsx
@@ -6,6 +6,55 @@ import { Typography } from 'antd';
 
 const { Title } = Typography;
 
+function NameToIcon(name) {
+
+  const val = name.toLowerCase();
+
+  let icon = null;
+  switch (val) {
+    case 'arduino':
+      break;
+    case 'atmel':
+      break;
+    case 'linux':
+      break;
+    case 'ROS':
+      break;
+    case 'ROS2':
+      break;
+    case 'c':
+    case 'c++':
+    case 'c/c++':
+      break;
+    case 'python':
+      break;
+    case 'matlab':
+      break;
+    case 'unity':
+      break;
+    case 'c#':
+      break;
+    case 'hololens':
+      break;
+    case 'git':
+      break;
+    case 'node':
+    case 'nodejs':
+      break;
+    case 'react':
+      break;
+    case 'javascript':
+      break;
+    case 'java':
+      break;
+
+    default:
+      break;
+  }
+
+  return icon;
+}
+
 
 function SectionSkills(props) {
 
--- src/content/skills.jsx
+++ src/content/skills.jsx
@@ -1,5 +1,251 @@
 data = {
-
+  "skills" : [
+    {
+      "name": "Arduino",
+      "progress": 100,
+      "notes": [
+        "Internship - Used for prototyping and debugging.",
+        "Personal Projects - Microcontroller framework of choice"
+      ],
+      "related": [
+        "Embedded System Development"
+      ]
+    },
+    {
+      "name": "Atmel",
+      "progress": 70,
+      "notes": [
+        "Tool - Atmel Studio",
+        "Internship - Developed server-mainboard firmware",
+        "Internship - Developed custom server fan-controller firmware",
+        "Coursework - Undergrad"
+      ],
+      "related": [
+        "Embedded System Development"
+      ]
+    },
+    {
+      "name": "Linux",
+      "progress": 70,
+      "notes": [
+        "Coursework - Undergrad",
+        "Coursework - Graduate",
+        "Research - Used in conjunction with ROS"
+      ],
+      "related": [
+        "ROS"
+      ]
+    },
+    {
+      "name": "Embedded System Development",
+      "progress": 90,
+      "notes": [
+        "Coursework - Undergrad",
+        "Internship",
+        "Personal Projects"
+      ],
+      "related": [
+        "Arduino",
+        "Atmel",
+        "C/C++",
+        "Git"
+      ]
+    },
+    {
+      "name": "ROS",
+      "progress": 80,
+      "notes": [
+        "Research - Backbone of my project codebase",
+        "Personal Projects - Used on hobby robot with a Nvidia Jetson Nano"
+      ],
+      "related": [
+        "Linux",
+        "Python",
+        "Backend Software Development",
+        "Universal Robots"
+      ]
+    },
+    {
+      "name": "C/C++",
+      "progress": 70,
+      "notes": [
+        "Primarily familiar with C",
+        "Internship - Embedded firmware was in C",
+        "Personal Projects - Use Arduino (derivative of C++)",
+        "Coursework - Undergrad"
+      ],
+      "related": [
+        "Embedded System Development"
+      ]
+    },
+    {
+      "name": "Python",
+      "progress": 80,
+      "notes": {
+        "Coursework - Graduate",
+        "Internship - Interaction between server and low-level firmware",
+        "Research - Primary language of my codebase",
+        "Personal Projects - Used either with ROS or for a Flask Server"
+      },
+      "related": [
+        "ROS",
+        "Backend Software Development"
+      ]
+    },
+    {
+      "name": "Backend Software Development",
+      "progress": 85,
+      "notes": [
+        "Coursework - Undergrad",
+        "Research",
+        "Internship"
+      ],
+      "related": [
+        "Python",
+        "ROS",
+        "Linux",
+        "Java",
+        "Nodejs",
+        "Git"
+      ]
+    },
+    {
+      "name": "Java",
+      "progress": 60,
+      "notes": [
+        "Coursework - Undergrad"
+      ],
+      "related": [
+        "Backend Software Development"
+      ]
+    },
+    {
+      "name": "Nodejs",
+      "progress": 50,
+      "notes": [
+        "Internship - Wrote custom server for a thermal chamber automation (IIOT) project"
+      ],
+      "related": [
+        "Backend Software Development",
+        "Javascript/Typescript"
+      ]
+    },
+    {
+      "name": "Git",
+      "progress": 55,
+      "notes": [
+        "Suffice to say this is my go to versioning tool"
+      ],
+      "related": [
+        "Backend Software Development",
+        "Frontend Software Development",
+        "Embedded System Development"
+      ]
+    },
+    {
+      "name": "Universal Robots",
+      "progress": 70,
+      "notes": [
+        "Research - Authr utilizes UR robots in simulation",
+        "Research - Task Interdependence used a UR3e",
+        "Research - Current work on operator training uses a UR3e",
+        "Research - Familiar with URScript and Polyscope development",
+        "Coursework - Graduate - Redesigned aspects of Polyscope for an HCI Class"
+      ],
+      "related": [
+        "ROS"
+      ]
+    },
+    {
+      "name": "Frontend Software Development",
+      "progress": 75,
+      "notes": [
+        "Coursework - Undergrad",
+        "Coursework - Graduate",
+        "Research",
+        "Personal Projects"
+      ],
+      "related": [
+        "Unity",
+        "Hololens",
+        "React",
+        "Angular"
+      ]
+    },
+    {
+      "name": "Javascript/Typescript",
+      "progress": 45,
+      "notes": [
+        "I am grouping HTML/CSS into this as well",
+        "Generally forget the specifics of this language"
+      ],
+      "related": [
+        "React",
+        "Angular",
+        "Nodejs"
+      ]
+    },
+    {
+      "name": "Unity",
+      "progress": 60,
+      "notes": [
+        "Research - Used for Hololens development",
+        "Research - Used for robot simulations / visualizations",
+        "Personal Projects - Some minor game development"
+      ],
+      "related": [
+        "CSharp",
+        "Frontend Software Development",
+        "Hololens",
+        "ROS"
+      ]
+    },
+    {
+      "name": "CSharp",
+      "progress": 60,
+      "notes": [
+        "Only worked within it in the context of Unity"
+      ],
+      "related": [
+        "Unity"
+      ]
+    },
+    {
+      "name": "Hololens",
+      "progress": 50,
+      "notes": [
+        "Research - Robot visualizations for operator training"
+      ],
+      "related": [
+        "Unity",
+        "Frontend Software Development"
+      ]
+    },
+    {
+      "name": "React",
+      "progress": 60,
+      "notes": [
+        "Research - Operator training interfaces",
+        "Personal Projects - My resume website was developed using this framework"
+      ],
+      "related": [
+        "Frontend Software Development",
+        "Javascript/Typescript"
+      ]
+    },
+    {
+      "name": "Angular",
+      "progress": 60,
+      "notes": [
+        "Research - Authr's interface was developed using this framework",
+        "Coursework - Undergrad senior design used this framework"
+      ],
+      "related": [
+        "Frontend Software Development",
+        "Javascript/Typescript"
+      ]
+    }
+  ]
 };
 
 export default data;
