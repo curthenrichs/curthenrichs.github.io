31839d56e78b36138f13bb2a24e17dd89b8e6883
Curt Henrichs <curthenrichs@gmail.com>
2022-12-15 21:34:27 -0800

    fixing footer sitemap wrapping for small screen widths

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,6 +1,7 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Typography, Divider } from "antd";
+import { WidthContext } from "../contexts";
 
 const { Text } = Typography;
 
@@ -9,13 +10,12 @@ const Spacer = () => {
 };
 
 const Copyright = () => {
-  return (
-    <Fragment>
-      <Divider type="horizontal" />
-      <Text strong>Curt Henrichs © {new Date().getFullYear()}</Text>
-      <br />
-      <Text>Created with Reactjs and Ant Design</Text>
-      <br />
+
+  const width = useContext(WidthContext);
+
+  let sitemap;
+  if (width >= 700) {
+    sitemap = (
       <Text>
         <Link to="/">Home</Link>
         <Spacer />
@@ -29,6 +29,38 @@ const Copyright = () => {
         <Spacer />
         <Link to="/attribution">Attribution</Link>
       </Text>
+    );
+  } else {
+    sitemap = (
+      <Fragment>
+        <br />
+        <Text>
+          <Link to="/">Home</Link>
+          <Spacer />
+          <Link to="/resume">Resume</Link>
+          <Spacer />
+          <Link to="/terms">Terms of Use</Link>
+        </Text>
+        <br />
+        <Text>
+          <Link to="/accessibility">Accessibility Policy</Link>
+          <Spacer />
+          <Link to="/privacy">Privacy Policy</Link>
+          <Spacer />
+          <Link to="/attribution">Attribution</Link>
+        </Text>
+      </Fragment>
+    );
+  }
+
+  return (
+    <Fragment>
+      <Divider type="horizontal" />
+      <Text strong>Curt Henrichs © {new Date().getFullYear()}</Text>
+      <br />
+      <Text>Created with Reactjs and Ant Design</Text>
+      <br />
+      {sitemap}
     </Fragment>
   );
 };
