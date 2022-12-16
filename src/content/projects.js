369daf6bffe9f65819888f4e787095f5ac4c5126
Curt Henrichs <curthenrichs@gmail.com>
2022-12-15 21:49:49 -0800

    stubbing website project content

--- src/content/projects.js
+++ src/content/projects.js
@@ -2,11 +2,13 @@ import AuthrModal from "./markdown/modal/AuthrProject.md";
 import ITERModal from "./markdown/modal/ITERProject.md";
 import CoFrameModal from "./markdown/modal/EVDProject.md";
 import HobbyModal from "./markdown/modal/HobbyProject.md";
+import WebsiteModal from "./markdown/modal/WebsiteProject.md";
 
 import AuthrBrief from "./markdown/brief/AuthrProject.md";
 import ITERBrief from "./markdown/brief/ITERProject.md";
 import CoFrameBrief from "./markdown/brief/EVDProject.md";
 import HobbyBrief from "./markdown/brief/HobbyProject.md";
+import WebsiteBrief from "./markdown/brief/WebsiteProject.md";
 
 const data = {
   "project-authr": {
@@ -144,112 +146,20 @@ const data = {
       }
     ],
     publications: []
+  },
+  "project-website": {
+    title: "Portfolio Website",
+    brief: "Background on the development for this portfolio webiste",
+    descriptionMarkdownPath: WebsiteBrief,
+    thumbnail: "",
+    type: "Personal",
+    _id: "project-website",
+    modalMarkdownPath: WebsiteModal,
+    notable: false,
+    skills: [],
+    images: [],
+    publications: []
   }
 };
 
 export default data;
-
-// Old data no longer needed but don't want to throw away just yet
-
-// import ComputerVisionModal from "./markdown/modal/VisionMLProject.md";
-// import EmbeddedModal from "./markdown/modal/EmbeddedProject.md";
-// import HCIModal from "./markdown/modal/HCIProject.md";
-
-// import ComputerVisionBrief from "./markdown/brief/VisionMLProject.md";
-// import EmbeddedBrief from "./markdown/brief/EmbeddedProject.md";
-// import HCIBrief from "./markdown/brief/HCIProject.md";
-
-/*
-"project-vision": {
-    "title": "Computer Vision and ML Projects",
-    "brief": "Various computer vision and machine learning projects from coursework.",
-    "descriptionMarkdownPath": ComputerVisionBrief,
-    "thumbnail": "/img/teaser-computer-vision.png",
-    "type": "Coursework",
-    "_id": "project-vision",
-    "modalMarkdownPath": ComputerVisionModal,
-    "notable": false,
-    "skills": [],
-    "images": [
-      {
-        "img": "/img/computer-vision-iter.png",
-        "alt": "",
-        "caption": "ITER computer vision spinoff detects magnetic blocks in 3D space using AR markers."
-      },
-      {
-        "img": "/img/computer-vision-gan-learning.gif",
-        "alt": "",
-        "caption": "Sprite GAN learning body armor."
-      },
-      {
-        "img": "/img/computer-vision-gan-single.gif",
-        "alt": "",
-        "caption": "Sprite GAN learning body armor - single example."
-      }
-    ],
-    "publications": []
-  },
-  "project-embedded": {
-    "title": "Embedded System Projects",
-    "brief": "Various embedded systems projects from my undergrad coursework.",
-    "descriptionMarkdownPath": EmbeddedBrief,
-    "thumbnail": "/img/teaser-embedded.jpg",
-    "type": "Coursework",
-    "_id": "project-embedded",
-    "modalMarkdownPath": EmbeddedModal,
-    "notable": false,
-    "skills": [],
-    "images": [
-      {
-        "img": "/img/embedded-networking.jpg",
-        "alt": "",
-        "caption": "Networking one project hardware where I am using a Cypress SOC."
-      },
-      {
-        "img": "/img/embedded-treadmill.jpg",
-        "alt": "",
-        "caption": "Embedded Systems IV course project. Firmware implementation for a treadmill on a Cypress SOC."
-      },
-      {
-        "img": "/img/embedded-tracking.jpg",
-        "alt": "",
-        "caption": "Embedded Systems III course project. Uses camera to track hand on NIOS soft-core processor."
-      },
-      {
-        "img": "/img/embedded-pcb.jpg",
-        "alt": "",
-        "caption": "Embedded Systems III homework. I implemented a Atmel microcontroller PCB in Eagle."
-      }
-    ],
-    "publications": []
-  },
-  "project-hci": {
-    "title": "HCI and Data Visualization",
-    "brief": "A few HCI and data visualization course projects.",
-    "descriptionMarkdownPath": HCIBrief,
-    "thumbnail": "/img/teaser-hci.jpg",
-    "type": "Coursework",
-    "_id": "project-hci",
-    "modalMarkdownPath": HCIModal,
-    "notable": false,
-    "skills": [],
-    "images": [
-      {
-        "img": "/img/hci-hifi-mockup.jpg",
-        "alt": "",
-        "caption": "High fidelity mockup of my Universal Robots teach pendant redesign in Adobe XD."
-      },
-      {
-        "img": "/img/hci-vis-overview.jpg",
-        "alt": "",
-        "caption": "Overview interactive visualization of Amazon product categories created by Plotly in Python."
-      },
-      {
-        "img": "/img/hci-vis-detail.jpg",
-        "alt": "",
-        "caption": "Detail interactive visualization of Amazon product categories created by Plotly in Python."
-      }
-    ],
-    "publications": []
-  }
-*/
