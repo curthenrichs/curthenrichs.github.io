c9e1d046f6c92bfe8ef733ae62c6bd7fac6b0209
Curt Henrichs <curthenrichs@gmail.com>
2023-01-07 14:32:22 -0800

    reworking image carousel styling to be less clunky

--- src/components/ImageCarousel/index.jsx
+++ src/components/ImageCarousel/index.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import "./index.css";
 
 const ImageCarousel = (props) => {
   const { options } = props;
 
   return (
-    <Carousel autoPlay infiniteLoop interval={5000} emulateTouch className="carousel">
+    <Carousel 
+        autoPlay 
+        infiniteLoop 
+        interval={5000} 
+        emulateTouch 
+        className="carousel"
+        stopOnHover={true}
+        showStatus={false}
+    >
       {options.map((entry, idx) => (
         <div key={idx} style={{ display: "flex", justifyContent: "center" }}>
           <img
